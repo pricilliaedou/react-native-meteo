88979b1bbf09aff2a8ee4551440409927143da09
pricilliaedou <pricillia.edou@gmail.com>
2026-02-23 22:56:31 +0100

    page home

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
+import "react-native-reanimated";
+
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)/Home/Home" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
