e3cab47c4ae11656cfb13de5fa94a5fb53356f0e
pricilliaedou <pricillia.edou@gmail.com>
2026-02-27 16:39:08 +0100

    Ajout de la demande de permission de localisation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,35 @@
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [coords, setCoords] = useState<null | {
+    lat: number;
+    lon: number;
+  }>(null);
+
+  useEffect(() => {
+    getUsercoords();
+  }, []);
+
+  async function getUsercoords() {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoords({
+        lat: location.coords.latitude,
+        lon: location.coords.longitude,
+      });
+    } else {
+      setCoords({ lat: 48.8566, lon: 2.3522 });
+    }
+  }
+
+  console.log(coords);
+
   return (
     <>
       <View style={styles.meteo_basic}>
