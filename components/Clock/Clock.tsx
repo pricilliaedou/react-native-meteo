d3473cdf0bed81f004f4b0a6df9b0b349040c922
pricilliaedou <pricillia.edou@gmail.com>
2026-03-13 12:11:18 +0100

    Mise à jour automatique de l'heure

--- components/Clock/Clock.tsx
+++ components/Clock/Clock.tsx
@@ -1,7 +1,17 @@
 import { nowToHHMM } from "@/services/date-service";
+import { useEffect, useState } from "react";
 import Txt from "../Txt/Txt";
 import { styles } from "./Clock.style";
 
 export default function Clock() {
+  const [time, setTime] = useState(nowToHHMM());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(nowToHHMM());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return <Txt style={styles.time}>{nowToHHMM()}</Txt>;
 }
