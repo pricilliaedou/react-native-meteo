131133ccb734fec5d2c4e467d5e5b7a4e5d4b3aa
pricilliaedou <pricillia.edou@gmail.com>
2026-03-20 16:01:37 +0100

    ajout des icones

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { IconSymbol } from "@/components/ui/icon-symbol";
 
 export default function TabLayout() {
   return (
@@ -19,6 +20,9 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
